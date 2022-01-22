def5b1940b0b6b3bf95ad00d7e4247b82bfa6152
wyork <w.york63@gmail.com>
2022-01-22 10:57:07 -0500

    function to write readme and initialize app

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Successfully wrote: " + fileName)
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("README.md", generateMarkdown(data));
+    })
+
+}
 
 // Function call to initialize app
 init();
